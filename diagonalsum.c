3b66239b8a398a2cda4ddb490cd99bfd32601481
MandadiSrinivasReddy <64724982+MandadiSrinivasReddy@users.noreply.github.com>
2020-05-22 17:22:58 +0530

    create diagonalsum.c

---
+++ diagonalsum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,a[3][3],sum=0;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+		for(i=0;i<3;i++)
+	{
+			for(j=0;j<3;j++)
+		{
+			if(i==j)
+			{
+		sum=sum+*(*(a+i)+j);
+	}
+	}
+}
+	printf("the sum of diagonal elementsis %d",sum);
+	return 0;
+}
+	
